cbe779243a919609eb146d9995ada809f74feb2d
AlecRoss01 <61425095+AlecRoss01@users.noreply.github.com>
2022-11-26 11:14:12 -0500

    code is functional now, need it to be integrated into a button or something else

--- lib/client.dart
+++ lib/client.dart
@@ -4,21 +4,20 @@ import 'dart:async';
 
 //https://stackoverflow.com/questions/54481818/how-to-connect-flutter-app-to-tcp-socket-server
 
-tcpClient() async {
+main() async {
+  // Start of core TCP connection
   Socket socket = await Socket.connect('127.0.0.1', 30000);
   print('connected');
-
-  // send hello
-  socket.add(utf8.encode('GETMENU'));
+  // send GETMENU, which will get back a list of bytes that have the menu in them
+  socket.add(utf8.encode('GETMENU\n'));
 
   // listen to the received data event stream
   socket.listen((List<int> event) {
     print(utf8.decode(event));
   });
 
-  // wait 5 seconds
-  await Future.delayed(Duration(seconds: 5));
-
+  socket.add(utf8.encode('STOP\n'));
   // .. and close the socket
   socket.close();
+  //End of core TCP connection
 }
