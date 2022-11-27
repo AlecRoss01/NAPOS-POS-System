e244152c7a36306650ce6ac13078cf8e59dad670
AlecRoss01 <61425095+AlecRoss01@users.noreply.github.com>
2022-11-27 14:56:26 -0500

    added helper functions needed for the fronted usability

--- lib/client.dart
+++ lib/client.dart
@@ -1,23 +1,65 @@
 import 'dart:io';
 import 'dart:convert';
 import 'dart:async';
+import 'menu_item.dart';
+import 'order.dart';
+
 
 //https://stackoverflow.com/questions/54481818/how-to-connect-flutter-app-to-tcp-socket-server
 
 main() async {
-  // Start of core TCP connection
+  //var menu = await recvMenu();
+  //print(menu);
+  var order = Order(0, 'lambchops');
+  var id = await sendOrder(order);
+  print(id);
+}
+
+
+// https://stackoverflow.com/questions/63323038/dart-return-data-from-a-stream-subscription-block
+recvMenu() async {
+  // returns the menu as a list of strings
+  var menuList = [];
   Socket socket = await Socket.connect('127.0.0.1', 30000);
   print('connected');
-  // send GETMENU, which will get back a list of bytes that have the menu in them
   socket.add(utf8.encode('GETMENU\n'));
-
-  // listen to the received data event stream
-  socket.listen((List<int> event) {
-    print(utf8.decode(event));
-  });
-
-  socket.add(utf8.encode('STOP\n'));
-  // .. and close the socket
+  await for (var data in socket){
+    //print(utf8.decode(data));
+    menuList.add(MenuItem(0, utf8.decode(data)));
+  }
   socket.close();
-  //End of core TCP connection
+  return menuList;
 }
+
+
+Future<int> sendOrder(Order order) async {
+  //Sends order and adds it to order db
+  Socket socket = await Socket.connect('127.0.0.1', 30000);
+    print('connected');
+    socket.add(utf8.encode('SENDORDER\n'));
+    socket.add(utf8.encode(order.orderIDNullChar + '\n'));
+    await for (var data in socket){
+        //print(utf8.decode(data));
+        if (utf8.decode(data) == "finish") {
+            socket.close();
+            return 0;
+        }
+    }
+    socket.close();
+    return 0;
+}
+
+recvOrders() async {
+  // returns all orders in the order db
+  var ordersList = [];
+  Socket socket = await Socket.connect('127.0.0.1', 30000);
+  print('connected');
+  socket.add(utf8.encode('GETORDERS\n'));
+  await for (var data in socket){
+      //print(utf8.decode(data));
+      ordersList.add(Order(0, utf8.decode(data)));
+  }
+  socket.close();
+  return ordersList;
+}
+
