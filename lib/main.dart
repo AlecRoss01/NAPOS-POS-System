440d1a282bc49ed3e4a747cd75a2756dae8afe20
reece-cristea <reece.cristea@mymail.champlain.edu>
2023-02-28 21:39:06 -0500

    added more functionality to login screen, now can input numbers and delete them, next i'll be working on checking if inputed pin number is in the system to let user login

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,42 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  
+  var pin = <int>[];
+
+  void addNumToPIN(String number){
+    setState(() {
+      if (pin.length == 4){
+        return;
+      }
+      else {
+        pin.add(int.parse(number));
+      }
+    });
+  }
+
+  void delLastPINNumber() {
+    setState(() {
+      if (!pin.isEmpty)
+      {
+        pin.removeLast();
+      }
+    });
+  }
+
+  String pinToString() {
+    String returnString = "";
+    if (pin.isEmpty){
+      return returnString;
+    }
+    for (int i = 0; i < pin.length; i++)
+    {
+      returnString += pin[i].toString();
+    }
+    return returnString;
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,36 +80,46 @@ class _LoginScreenState extends State<LoginScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget> [
+              Text(pinToString()),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget> [
-                  PinNumber(number: "1"),
+                  PinNumber(number: "1", addNumberToPIN: addNumToPIN),
                   SizedBox(width: 10),
-                  PinNumber(number: "2"),
+                  PinNumber(number: "2", addNumberToPIN: addNumToPIN),
                   SizedBox(width: 10),
-                  PinNumber(number: "3")
+                  PinNumber(number: "3", addNumberToPIN: addNumToPIN)
                 ]
               ),
               SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget> [
-                  PinNumber(number: "4"),
+                  PinNumber(number: "4", addNumberToPIN: addNumToPIN),
                   SizedBox(width: 10),
-                  PinNumber(number: "5"),
+                  PinNumber(number: "5", addNumberToPIN: addNumToPIN),
                   SizedBox(width: 10),
-                  PinNumber(number: "6")
+                  PinNumber(number: "6", addNumberToPIN: addNumToPIN)
                 ]
               ),
               SizedBox(height: 10),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget> [
-                  PinNumber(number: "7"),
+                  PinNumber(number: "7", addNumberToPIN: addNumToPIN),
                   SizedBox(width: 10),
-                  PinNumber(number: "8"),
+                  PinNumber(number: "8", addNumberToPIN: addNumToPIN),
                   SizedBox(width: 10),
-                  PinNumber(number: "9")
+                  PinNumber(number: "9", addNumberToPIN: addNumToPIN)
+                ]
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget> [
+                  PinNumber(number: "0", addNumberToPIN: addNumToPIN),
+                  SizedBox(width: 10),
+                  DelNumber(delSymbol: "DEL", removeLastNum: delLastPINNumber),
                 ]
               ),
               SizedBox(height: 10),
@@ -92,6 +138,9 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 onPressed: () {
                   //check login credentials here, if they pass got to POS Home Page
+                  setState(() {
+                    pin.clear();
+                  });
                   Navigator.push(context, MaterialPageRoute(builder: (context) => const POSHomePage(title: 'Napos POS')));
                 }
               )
@@ -103,15 +152,19 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 }
 
-class PinNumber extends StatelessWidget {
+class PinNumber extends StatefulWidget {
   final String number;
-  const PinNumber({super.key, required this.number});
+  final Function(String) addNumberToPIN;
+  const PinNumber({super.key, required this.number, required this.addNumberToPIN});
 
-  // This widget is the root of the application.
+  State<PinNumber> createState() => _PinNumberState();
+}
+
+class _PinNumberState extends State<PinNumber> {
   @override
   Widget build(BuildContext context) {
     return TextButton(
-      child: Text(number, style:CustomTextStyle.homeButtons),
+      child: Text(widget.number, style:CustomTextStyle.homeButtons),
       style: ButtonStyle(
         shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
@@ -121,6 +174,35 @@ class PinNumber extends StatelessWidget {
         )
       ),
       onPressed: () {
+        widget.addNumberToPIN(widget.number);
+      }
+    );
+  }
+}
+
+class DelNumber extends StatefulWidget {
+  final String delSymbol;
+  final Function() removeLastNum;
+  const DelNumber({super.key, required this.delSymbol, required this.removeLastNum});
+
+  State<DelNumber> createState() => _DelNumberState();
+}
+
+class _DelNumberState extends State<DelNumber> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      child: Text(widget.delSymbol, style:CustomTextStyle.homeButtons),
+      style: ButtonStyle(
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18.0),
+            side: BorderSide(color: Colors.white)
+          )
+        )
+      ),
+      onPressed: () {
+        widget.removeLastNum();
       }
     );
   }
