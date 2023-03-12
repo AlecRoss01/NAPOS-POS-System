aecc930fdb8e4044bf8491260414c17239426fdf
jacob-myers <jakemyers190@gmail.com>
2023-03-12 16:38:51 -0400

    Redoing structure for home page. Making the buttons a new widget. Adding basic pieces for the Kitchen Endpoint page.

--- lib/pages/pos_home_page.dart
+++ lib/pages/pos_home_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:napos/widgets/home_page_button.dart';
 import '../styles/styles.dart';
 import 'command_hub.dart';
 import 'analytics_hub.dart';
+import 'kitchen_endpoint.dart';
 
 // This widget is the home page of the POS application.
 class POSHomePage extends StatefulWidget {
@@ -35,44 +37,44 @@ class _POSHomePage extends State<POSHomePage> {
           SizedBox(width: 10,),
         ],
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center, // Align buttons to center of the row
-          children: <Widget>[
-            // Sized boxes wrap buttons so they can be sized
-            // First Button, for POS page
-            SizedBox(
-              height: 100,
-              width: 200,
-              child: TextButton(
-                child: const Text("Point of Sale", style:CustomTextStyle.homeButtons),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const CommandHub())
-                  );
-                },
+      body: Column(
+        children: [
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center, // Align buttons to center of the row
+            children: [
+              // First Button, for POS page
+              HomePageButton(
+                text: 'Point of Sale',
+                targetPage: () => CommandHub(),
               ),
-            ),
-            // Space between buttons
-            SizedBox(width: 50,),
-            // Second Button, for Analytics Hub page
-            SizedBox(
-              height: 100,
-              width: 200,
-              child: TextButton(
-                child: const Text("Analytics Hub", style:CustomTextStyle.homeButtons),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const AnalyticsHub())
-                  );
-                },
+
+              SizedBox(width: 50),
+
+              // Second button, for Analytics Hub page.
+              HomePageButton(
+                text: 'Analytics Hub',
+                targetPage: () => AnalyticsHub(),
               ),
-            ),
-          ],
-        )
-      ),
+
+            ],
+          ),
+
+          SizedBox(height: 50),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              HomePageButton(
+                text: 'Kitchen Endpoint',
+                targetPage: () => KitchenEndpointPage(),
+              ),
+            ],
+          ),
+
+          Spacer()
+        ],
+      )
     );
   }
 }
