cb63106a0cdf4d947a97769af2862c955524fd52
Alec Ross <alec.ross@mymail.champlain.edu>
2023-03-26 13:03:55 -0400

    made it so that it doesn't flood server

--- lib/pages/kitchen_endpoint.dart
+++ lib/pages/kitchen_endpoint.dart
@@ -27,9 +27,13 @@ class _KitchenEndpointPage extends State<KitchenEndpointPage> {
 
   @override
   void initState() {
-    const Duration oneSecond = Duration(seconds: 1);
+    const Duration oneSecond = Duration(seconds: 2);
     const Duration oneMinute = Duration(minutes: 1);
-    myUpdater = Timer.periodic(oneSecond, (Timer t) => setState(() {}));
+    myUpdater = Timer.periodic(
+        oneSecond,
+        (Timer t) => setState(() {
+              updateOrders();
+            }));
   }
 
   @override
@@ -48,7 +52,7 @@ class _KitchenEndpointPage extends State<KitchenEndpointPage> {
     completeOrders = await recvCompleteOrders();
     inCompleteOrders = await recvIncompleteOrders();
     // Call to rebuild.
-    setState(() {});
+    //setState(() {});
 
     //orders = newOrders;
     // Remove all
@@ -73,80 +77,82 @@ class _KitchenEndpointPage extends State<KitchenEndpointPage> {
     updateOrders();
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Kitchen Endpoint'),
-      ),
-
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            // Toggle between to-do and incomplete.
-            Row(
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 50,
-                    child: TextButton(
-                      child: Text('To Do', style:CustomTextStyle.kitchenEndpointToggles, textAlign: TextAlign.center,),
-                      onPressed: () {
-                        setToDo(true);
-                      },
-                    ),
-                  ),
-                ),
-
-                Expanded(
-                  child:SizedBox(
-                    height: 50,
-                    child: TextButton(
-                      child: Text('Completed', style:CustomTextStyle.kitchenEndpointToggles, textAlign: TextAlign.center,),
-                      onPressed: () {
-                        setToDo(false);
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-
-            // List of orders.
-            Expanded(
-              child: Column(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Kitchen Endpoint'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: [
+              // Toggle between to-do and incomplete.
+              Row(
                 children: [
                   Expanded(
-                    child: FutureBuilder<List<Order>>(
+                    child: SizedBox(
+                      height: 50,
+                      child: TextButton(
+                        child: Text(
+                          'To Do',
+                          style: CustomTextStyle.kitchenEndpointToggles,
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {
+                          setToDo(true);
+                        },
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      height: 50,
+                      child: TextButton(
+                        child: Text(
+                          'Completed',
+                          style: CustomTextStyle.kitchenEndpointToggles,
+                          textAlign: TextAlign.center,
+                        ),
+                        onPressed: () {
+                          setToDo(false);
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              // List of orders.
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: FutureBuilder<List<Order>>(
                       //TODO: HERE, WE NEED TO DISTINGUISH BETWEEN TODO, AND FINISHED ORDERS.
                       future: getOrders(),
                       builder: (context, snapshot) {
-                        if(snapshot.hasData) {
+                        if (snapshot.hasData) {
                           return ListView.separated(
                             scrollDirection: Axis.vertical,
                             //shrinkWrap: true,
                             itemCount: snapshot.data!.length,
                             itemBuilder: (context, index) {
-                              return OrderDisplayWidget(order: snapshot.data![index]);
+                              return OrderDisplayWidget(
+                                  order: snapshot.data![index]);
                             },
                             separatorBuilder: (context, index) {
                               return Divider();
                             },
                           );
-                        }
-                        else {
+                        } else {
                           return Text('No data');
                         }
                       },
-                    )
-                  )
-                ],
-              ),
-            )
-
-          ],
-        ),
-      )
-    );
+                    ))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
-
 }
