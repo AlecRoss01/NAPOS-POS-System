dd7c88d5ac923f8a2a742c44f065ce9faaa3e3c3
Alec Ross <alec.ross@mymail.champlain.edu>
2023-04-06 19:44:15 -0400

    added json encoding and decoding

--- lib/classes/employee.dart
+++ lib/classes/employee.dart
@@ -1,13 +1,37 @@
 class NAPOS_Employee {
-    String _empName;
-    int _empID;
-    int _empPIN;
-    int _accessLevel;
+  String _empName;
+  int _empID;
+  int _empPIN;
+  int _accessLevel;
 
-    NAPOS_Employee(this._empName, this._empID, this._empPIN, this._accessLevel);
+  NAPOS_Employee(this._empName, this._empID, this._empPIN, this._accessLevel);
 
-    String get name { return _empName; }
-    int get id { return _empID; }
-    int get pin { return _empPIN; }
-    int get access { return _accessLevel; }
+  String get name {
+    return _empName;
+  }
+
+  int get id {
+    return _empID;
+  }
+
+  int get pin {
+    return _empPIN;
+  }
+
+  int get access {
+    return _accessLevel;
+  }
+
+  NAPOS_Employee.fromJson(Map<String, dynamic> json)
+      : _empName = json['Name'],
+        _empID = json['ID'],
+        _empPIN = json['PIN'],
+        _accessLevel = json['Access'];
+
+  Map<String, dynamic> toJson() => {
+        'Name': _empName,
+        'ID': _empID,
+        'PIN': _empPIN,
+        'Access': _accessLevel,
+      };
 }
